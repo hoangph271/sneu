efe8317b9031e4014bcb38a3c2e9c1e589706977
Phan Huy Hoang <hoangph271@gmail.com>
2022-06-30 01:40:35 +0700

    [Clean]

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,13 +1,10 @@
 /** @jsx h */
 import { h } from '$fresh/runtime.ts'
-import Clock from '../islands/Clock.tsx'
-import InitStyles from '../islands/InitStyles.tsx'
 
 const Home = () => {
   return (
     <div style="display: flex; flex-direction: column; align-items: center;">
-      <Clock />
-      <InitStyles />
+      @sneu
     </div>
   )
 }
